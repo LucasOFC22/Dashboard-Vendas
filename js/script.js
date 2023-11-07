4789ac8a712ccf1bb6030c02196be585421210d8
LucasOFC22 <150047061+LucasOFC22@users.noreply.github.com>
2023-11-06 22:16:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, query, where, getDocs } from 'firebase/firestore';
+
+// Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAhsgy-3hgII6JVv91m6HBkOatM84br7TI",
     authDomain: "dashboard-de-vendas-a9c6a.firebaseapp.com",
@@ -6,83 +10,98 @@ const firebaseConfig = {
     messagingSenderId: "1031844698931",
     appId: "1:1031844698931:web:5f79e7b14f4c4ac399fc46",
     measurementId: "G-B7GSHQ10J5"
-  };
-  import { initializeApp } from 'firebase/app';
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-    const addSaleForm = document.getElementById('add-sale-form');
-    const salesList = document.getElementById('sales-list');
-    const searchButton = document.getElementById('search-button');
-    const searchCustomer = document.getElementById('search-customer');
+// Inicialize o Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-    const salesCollection = collection(db, 'sales');
-    const querySnapshot =  getDocs(salesCollection);
+// Elementos do formulário
+const addSaleForm = document.getElementById('add-sale-form');
+const salesList = document.getElementById('sales-list');
+const searchButton = document.getElementById('search-button');
+const searchCustomer = document.getElementById('search-customer');
+
+// Adiciona uma venda ao Firestore
+addSaleForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const customer = document.getElementById('customer').value;
+    const product = document.getElementById('product').value;
+    const value = document.getElementById('value').value;
+    const address = document.getElementById('address').value;
+    const date = document.getElementById('calendar').value;
+    const salesperson = document.getElementById('salesperson').value;
+
+    // Objeto para representar a venda
+    const sale = {
+        customer,
+        product,
+        value,
+        address,
+        date,
+        salesperson,
+    };
+
+    try {
+        // Adiciona a venda ao Firestore
+        const docRef = await addDoc(collection(db, 'sales'), sale);
+        console.log('Venda adicionada com ID: ', docRef.id);
+
+        // Limpa o formulário
+        addSaleForm.reset();
+    } catch (error) {
+        console.error('Erro ao adicionar a venda: ', error);
+    }
+});
+
+// Filtra as vendas por cliente
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchCustomer.value;
+    filterSalesByCustomer(searchTerm);
+});
+
+// Lista todas as vendas do Firestore
+async function listSales() {
+    const q = query(collection(db, 'sales'));
+    const querySnapshot = await getDocs(q);
+
     querySnapshot.forEach((doc) => {
         const sale = doc.data();
         appendSaleToDOM(sale);
     });
+}
 
-    addSaleForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const customer = document.getElementById('customer').value;
-        const product = document.getElementById('product').value;
-        const value = document.getElementById('value').value;
-        const address = document.getElementById('address').value;
-        const date = document.getElementById('calendar').value;
-        const salesperson = document.getElementById('salesperson').value;
-
-        // Criando um objeto para representar a venda
-        const sale = {
-            customer,
-            product,
-            value,
-            address,
-            date,
-            salesperson,
-        };
-
-        // Adicionando a venda ao Firebase Realtime Database
-        const newSaleRef = salesRef.push();
-        newSaleRef.set(sale);
-
-        addSaleForm.reset();
+// Filtra as vendas pelo nome do cliente
+function filterSalesByCustomer(customerName) {
+    const saleItems = document.querySelectorAll('.sale-item');
+    saleItems.forEach((saleItem) => {
+        const customerElement = saleItem.querySelector('.customer-name');
+        const customer = customerElement.textContent;
+        if (customer.toLowerCase().includes(customerName.toLowerCase())) {
+            saleItem.style.display = 'block';
+        } else {
+            saleItem.style.display = 'none';
+        }
     });
+}
 
-    searchButton.addEventListener('click', () => {
-        const searchTerm = searchCustomer.value;
-        filterSalesByCustomer(searchTerm);
-    });
+// Adiciona uma venda à lista
+function appendSaleToDOM(sale) {
+    const saleItem = document.createElement('li');
+    saleItem.classList.add('sale-item');
 
-    function appendSaleToDOM(sale) {
-        const saleItem = document.createElement('li');
-        saleItem.classList.add('sale-item');
+    saleItem.innerHTML = `
+        <p><strong>Cliente:</strong> <span class="customer-name">${sale.customer}</span></p>
+        <p><strong>Produto:</strong> ${sale.product}</p>
+        <p><strong>Valor:</strong> R$${sale.value}</p>
+        <p><strong>Endereço:</strong> ${sale.address}</p>
+        <p><strong>Data:</strong> ${sale.date}</p>
+        <p><strong>Vendedor:</strong> ${sale.salesperson}</p>
+    `;
 
-        saleItem.innerHTML = `
-            <p><strong>Cliente:</strong> <span class="customer-name">${sale.customer}</span></p>
-            <p><strong>Produto:</strong> ${sale.product}</p>
-            <p><strong>Valor:</strong> R$${sale.value}</p>
-            <p><strong>Endereço:</strong> ${sale.address}</p>
-            <p><strong>Data:</strong> ${sale.date}</p>
-            <p><strong>Vendedor:</strong> ${sale.salesperson}</p>
-        `;
+    salesList.appendChild(saleItem);
+}
 
-        salesList.appendChild(saleItem);
-    }
-
-    function filterSalesByCustomer(customerName) {
-        const saleItems = document.querySelectorAll('.sale-item');
-        saleItems.forEach((saleItem) => {
-            const customerElement = saleItem.querySelector('.customer-name');
-            const customer = customerElement.textContent;
-            if (customer.toLowerCase().includes(customerName.toLowerCase())) {
-                saleItem.style.display = 'block';
-            } else {
-                saleItem.style.display = 'none';
-            }
-        });
-    }
-});
-
+// Lista todas as vendas ao carregar a página
+listSales();
